c041a87db237b7611b0de1cf7ef644f84f7f6851
Keith <Keith.macario@gmail.com>
2021-11-11 00:13:25 -0800

    Created a dom funtion to create doms when called. This is tucked in a class.

---
+++ src/weatherDom.js
@@ -0,0 +1,11 @@
+class domGen {
+
+    divMaker(divType){
+        let container = document.querySelector(".container");
+        let div = document.createElement('div');
+        div.setAttribute("class", divType);
+        container.appendChild(div);
+    }
+}
+
+let humidity = domGen.divMaker(humidity);
