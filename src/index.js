f274d1900e892c55d28b748f69723ff676d46d5d
Keith <Keith.macario@gmail.com>
2021-11-11 18:36:46 -0800

    added styles css sheet.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { humidity  } from "./weatherDom";
+import './styles.css';
 
 const weatherInfo = (()=>{
     function getWeatherInfo(location){
@@ -20,11 +21,22 @@ const weatherInfo = (()=>{
 })();
 
 const weatherUI = (()=>{
+
     let weatherObject = weatherInfo.getWeatherInfo('London');
+
+            async function humidityControl(){
+                console.log("calling");
+                let humidity = await document.querySelector('humidity');
+                return humidity; 
+            }
+})();
+    
+ 
+ 
     
     // let weatherResult = JSON.parse(weatherObject.getWeatherInfo);
     // console.table(weatherResult);
-})();
+
 
 
 //http://api.openweathermap.org/data/2.5/weather?q=San%20Diego&APPID=cf59f533d7699be0364c16aa6a3d8741
