5456d020564a9d2c93439468760bb5636edb2a8f
Keith <Keith.macario@gmail.com>
2021-11-12 21:26:14 -0800

    Using prevent default on location form.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { domGenerator } from "./weatherDom";
 import './styles.css';
 
+const cityForm = document.getElementById("location");
+cityForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+});
+
 const pageLoad = (()=>{
     domGenerator.divMaker('humidity');
     console.log("calling")
-
-        checkIfSet(){
-            
-        }
-    checkIfSet()
 })();
 
 const weatherInfo = (()=>{
@@ -39,25 +39,8 @@ const weatherInfo = (()=>{
             getWeatherInfo,
         }
 })();
-
-
-const locationModule = (()=> {
-
-    let myLocation = document.getElementById("inputField");
-
-    myLocation.addEventListener('submit', ()=>{
-        function grabUserInput(){
-            let userInput = myLocation.value;
-            localStorage.setItem(userInput);
-            console.log(userInput);
-        }
-    })
-    
-    
-})();
 // weather info temp is called. 
 // input goes into the function call .
-
 
 
 //http://api.openweathermap.org/data/2.5/weather?q=San%20Diego&APPID=cf59f533d7699be0364c16aa6a3d8741
